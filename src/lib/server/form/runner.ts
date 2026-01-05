e624048a28dbd34124a03b4b9d231c0c6e1f3a91
Glorious Revolutionary Code Formatter <actions@github.com>
2026-01-05 11:12:04 +0000

    [AUTOMATIC] Code formatting

--- src/lib/server/form/runner.ts
+++ src/lib/server/form/runner.ts
@@ -1,27 +1,31 @@
-import type { Cookies, RequestEvent } from "@sveltejs/kit";
+import type { Cookies, RequestEvent } from '@sveltejs/kit';
 import { getUser } from '$lib/server/user/index.js';
 import { fail } from '@sveltejs/kit';
 
 export const checkFormData = (formData: FormData, array: []): boolean => {
-    for(let i = 0; i < array.length; i++) {
-        if(!formData.has(array[i])) return false;
-    }
-    return true;
-}
+	for (let i = 0; i < array.length; i++) {
+		if (!formData.has(array[i])) return false;
+	}
+	return true;
+};
 
 export const formRunner = async (
-    event: RequestEvent,
-    requiredFields: [],
-    runner: (event: RequestEvent, formData: FormData, cookies: Cookies) => Promise<void>
+	event: RequestEvent,
+	requiredFields: [],
+	runner: (
+		event: RequestEvent,
+		formData: FormData,
+		cookies: Cookies,
+	) => Promise<void>,
 ) => {
-    const user = await getUser(event);
+	const user = await getUser(event);
 	if (!user) return fail(401);
 
-    const formData = await event.request.formData();
-    if(!checkFormData(formData, requiredFields)) {
-        return fail(400);
-    }
+	const formData = await event.request.formData();
+	if (!checkFormData(formData, requiredFields)) {
+		return fail(400);
+	}
 
-    const value = await runner(event, formData, event.cookies);
-    return value;
+	const value = await runner(event, formData, event.cookies);
+	return value;
 };
--- src/routes/(root)/(login)/set/+page.server.ts
+++ src/routes/(root)/(login)/set/+page.server.ts
@@ -1,17 +1,10 @@
 import { formRunner } from '$lib/server/form/runner.js';
 
-
 export const load = async () => {};
 
 export const actions = {
 	setPassword: async (event) => {
-		await formRunner(
-			event, 
-			[], 
-			async () => {
-
-			}
-		)
+		await formRunner(event, [], async () => {});
 
 		//TODO
 	},
